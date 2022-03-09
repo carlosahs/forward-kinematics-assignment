dce7be088687ac2b1b8cb2bd66927da3a2fa330c
carlosahs <tacahs12@gmail.com>
2022-03-09 15:54:48 -0600

    Define assignment's functions

--- main.py
+++ main.py
@@ -5,48 +5,45 @@ from typing import List
 from typing import Tuple
 from typing_extensions import Self
 
+AXES = 3
+
 class Axis(Enum):
     X = 'x'
     Y = 'y'
     Z = 'z'
 
-Vector = List[float]
+def rot(axis: Axis, rad: float) -> np.ndarray:
+    crot = np.cos(rad)
+    srot = np.sin(rad)
 
-class Translation_Vector:
-    def __init__(self, x: float, y: float, z: float) -> None:
-        # 3 by 1 vector
-        self.vector = np.array([x, y, z]).reshape(3, 1)
+    mat = np.identity(AXES)
 
-class Rotation_Matrix:
-    def __init__(self, axis: Axis) -> None:
-        self.axis = axis
-        self.matrix = np.identity(3)
+    if axis == Axis.X:
+        mat[1][1] = crot
+        mat[2][2] = crot
 
-    def rot(self, rad: float) -> None:
-        crot = np.cos(rad)
-        srot = np.sin(rad)
+        mat[1][2] = -srot
+        mat[2][1] = srot
+    elif axis == Axis.Y:
+        mat[0][0] = crot
+        mat[2][2] = crot
 
-        if self.axis == Axis.X:
-            self.matrix[1][1] = crot
-            self.matrix[2][2] = crot
+        mat[2][0] = -srot
+        mat[0][2] = srot
+    elif axis == Axis.Z:
+        mat[0][0] = crot
+        mat[1][1] = crot
 
-            self.matrix[1][2] = -srot
-            self.matrix[2][1] = srot
-        elif self.axis == Axis.Y:
-            self.matrix[0][0] = crot
-            self.matrix[2][2] = crot
+        mat[0][1] = -srot
+        mat[1][0] = srot
 
-            self.matrix[2][0] = -srot
-            self.matrix[0][2] = srot
-        elif self.axis == Axis.Z:
-            self.matrix[0][0] = crot
-            self.matrix[1][1] = crot
-
-            self.matrix[0][1] = -srot
-            self.matrix[1][0] = srot
+    mat
 
 class Transformation_Matrix:
-    def __init__(self, rmatrix: Rotation_Matrix, tvector: Translation_Vector) -> None:
+    def __init__(self, rmatrix: np.ndarray, tvector: np.ndarray) -> None:
+        assert rmatrix.shape == (AXES, AXES)
+        assert tvector.shape == (AXES, 1)
+
         self.rmatrix = rmatrix
         self.tvector = tvector
 
@@ -56,18 +53,55 @@ class Transformation_Matrix:
             np.matmul(self.rmatrix, self.tvector) + tmatrix.tvector
         )
 
-def FK(L: Vector, q: Vector) -> Vector:
+    def inv(self) -> Self:
+        return Transformation_Matrix(
+            self.rmatrix.transpose(),
+            np.matmul(-self.rmatrix.transpose(), self.tvector)
+        ) 
+
+def FK(L: Tuple[int, int], q: Tuple[int, int]) -> Tuple[int, int]:
     """
     Solves the forward kinematics of a planar 2R robot.
 
     Returns:
-        @P (Vector): 2 by 1 vector containing the end effector's x and y 
+        @P (Tuple[int, int]): 2 by 1 vector containing the end effector's x and y 
             coordinates expressed in the base frame.
     """
 
-    P = []
+    q_sum = sum(q)
+
+    L1 = L[0]
+    L2 = L[1]
+
+    q1 = q[0]
+
+    P = (
+        L1 * np.cos(q_sum) + L1 * np.cos(q1),
+        L2 * np.sin(q_sum) + L2 * np.sin(q1)
+    )
 
     return P
+
+def GENFK(
+    a: np.ndarray, alpha: np.ndarray, d: np.ndarray, 
+    theta: np.ndarray, sigma: bool
+) -> Transformation_Matrix:
+    pass
+
+def plot_FK(fk: np.ndarray) -> None:
+    pass
+
+def rot2euler(R):
+    pass
+
+def euler2rot(A):
+    pass
+
+def rot2quat(R):
+    pass
+
+def quat2rot(Q):
+    pass
 
 if __name__ == '__main__':
     # TODO
