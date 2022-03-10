821c056742aba3acb0424484b5f079d5c4fb80a6
carlosahs <tacahs12@gmail.com>
2022-03-10 08:01:20 -0600

    Add helper method for testing of rotations

--- tests.py
+++ tests.py
@@ -8,21 +8,44 @@ from main import Transformation_Matrix
 
 ROTATION_SLICE = 100
 class Test_rot(unittest.TestCase):
-    def test_rot_x(self):
-        # Test every hundreth of a full rotation
+    def rot_helper(self, axis: Axis) -> None:
         for i in range(0, ROTATION_SLICE + 1):
             theta = 2 * i * np.pi / ROTATION_SLICE
 
             crot = np.cos(theta)
             srot = np.sin(theta)
             
-            expected = np.array([
-                1, 0, 0,
-                0, crot, -srot,
-                0, srot, crot
-            ]).reshape((AXES, AXES))
+            if axis == Axis.X:
+                expected = np.array([
+                    1, 0, 0,
+                    0, crot, -srot,
+                    0, srot, crot
+                ]).reshape((AXES, AXES))
+            elif axis == Axis.Y:
+                expected = np.array([
+                    crot, 0, srot,
+                    0, 1, 0,
+                    -srot, 0, crot
+                ]).reshape((AXES, AXES))
+            elif axis == Axis.Z:
+                expected = np.array([
+                    crot, -srot, 0,
+                    srot, crot, 0,
+                    0, 0, 1
+                ]).reshape((AXES, AXES))
+            else:
+                expected = np.empty((AXES, AXES))
 
-            self.assertTrue(np.array_equal(expected, rot(Axis.X, theta)))
+            self.assertTrue(np.array_equal(expected, rot(axis, theta)))
+
+    def test_rot_x(self):
+        self.rot_helper(Axis.X)
+
+    def test_rot_y(self):
+        self.rot_helper(Axis.Y)
+
+    def test_rot_z(self):
+        self.rot_helper(Axis.Z)
 
 class Test_Transformation_Matrix(unittest.TestCase):
     pass
