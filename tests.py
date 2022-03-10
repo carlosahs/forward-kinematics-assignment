8bf483943a7ddd9382f878d18c13a143d1d8a523
carlosahs <tacahs12@gmail.com>
2022-03-09 18:52:46 -0600

    Define initial tests

---
+++ tests.py
@@ -0,0 +1,18 @@
+import unittest
+
+from main import DH_Transformation_Matrix
+from main import FK
+from main import rot
+from main import Transformation_Matrix
+
+class Test_rot(unittest.TestCase):
+    pass
+
+class Test_Transformation_Matrix(unittest.TestCase):
+    pass
+
+class Test_DH_Transformation_Matrix(unittest.TestCase):
+    pass
+
+class Test_FK(unittest.TestCase):
+    pass
